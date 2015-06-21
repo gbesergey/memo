fe0707c636931b5c1cfb1f14552dc2d579f2c212
user <gbesergey@gmail.com>
2015-06-22 00:34:46 +0300

    input progress

--- js/input.js
+++ js/input.js
@@ -1,51 +1,73 @@
 function Input(graph) {
     var input = this;
-    this.graph = graph;
-    // MouseEvent, [WheelEvent], KeyboardEvent, TouchEvent
-    this.eventBuffer = [];
-    this.currentPatterns = [];
 
-    this.STARTING_STATES = {
+    input.graph = graph;
+    // MouseEvent, [WheelEvent], KeyboardEvent, TouchEvent
+    input.eventBuffer = [];
+    input.currentPatterns = [];
+
+    input.STARTING_STATE_FUNCTIONS = {
         NOT_SELECTED: function NOT_SELECTED() {
-            return memo.selection.length == 0;
+            return input.graph.selection.length == 0;
         },
         ONLY_NODES_SELECTED: function ONLY_NODES_SELECTED() {
-            return memo.selectedNodes.length != 0 && memo.selectedRelationships.length == 0;
+            return input.graph.selectedNodes.length != 0 && input.graph.selectedRelationships.length == 0;
         }
     };
 
-    this.InputPattern = new function () {
-        this.position = 0;
-    };
-
-    this.INPUT_PATTERNS = {
-        CLICK: Object.create(this.InputPattern, {
-            next : function (inputEvent) {
-
+    input.INPUT_PATTERN_MATCHERS = {
+        LEFT_CLICK: {
+            next: function (position, inputEvent) {
+                if (inputEvent instanceof MouseEvent && inputEvent.button == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }),
-        MOUSE_MOVE: Object.create(this.InputPattern, {
-            next : function (inputEvent) {
-
-            }
-        })
-    };
-
-    this.MANIPULATIONS = {
-        SELECT_NODE: function (eventBuffer) {
-
-        }
-    };
-
-    Input.prototype.consume = function (inputEvent) {
-        this.eventBuffer.push(inputEvent);
-    };
-
-    Input.prototype.next = function () {
-        for (var event in this.eventBuffer) {
-            for (var inputPattern in this.INPUT_PATTERNS) {
-                jQuery.extend({}, );
+        },
+        MOUSE_MOVE: {
+            next: function (position, inputEvent) {
+                if (inputEvent instanceof MouseEvent && (inputEvent.movementX != 0 || inputEvent.movementY != 0)) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         }
     };
+
+    input.MANIPULATIONS = {
+        SELECT_NODE: {
+            startingStateFunction: input.STARTING_STATE_FUNCTIONS.NOT_SELECTED,
+            getInputPatternProcessor: function () {
+                return {
+                    position: 0,
+                    inputMatcher: input.INPUT_PATTERN_MATCHERS.LEFT_CLICK,
+                    parametersCollector: {
+                        next: function(position, inputEvent) {
+                            if (position == 0) {
+                                this.x = inputEvent.screenX;
+                                this.y = inputEvent.screenY;
+                            }
+                        }
+                    },
+                    manipulation: function () {
+                        input.graph.selectNode(this.x, this.y);
+                    }
+                };
+            }
+        }
+    }
 }
+
+Input.prototype.consume = function (inputEvent) {
+    input.eventBuffer.push(inputEvent);
+};
+
+Input.prototype.next = function () {
+    for (var event in input.eventBuffer) {
+        for (var inputPattern in input.INPUT_PATTERN_MATCHERS) {
+            jQuery.extend({},{});
+        }
+    }
+};
